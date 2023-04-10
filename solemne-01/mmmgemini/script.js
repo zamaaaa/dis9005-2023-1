e280d339b9b16dacff9c0ea77371c96472d40562
mmmgemini <128399825+mmmgemini@users.noreply.github.com>
2023-04-10 13:02:37 -0400

    Add files via upload

---
+++ solemne-01/mmmgemini/script.js
@@ -0,0 +1,6 @@
+console.log("soy un computador");
+function myFunction() {
+    document.getElementById("demo").style.fontSize = "35px"; 
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "white";        
+  }
