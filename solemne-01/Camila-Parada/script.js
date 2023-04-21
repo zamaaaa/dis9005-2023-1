bae5b74016f16499dc65b9871f5bb3e5703d56e7
Camila-Parada <128399287+Camila-Parada@users.noreply.github.com>
2023-04-21 19:33:07 -0400

    Update script.js

--- solemne-01/Camila-Parada/script.js
+++ solemne-01/Camila-Parada/script.js
@@ -1,5 +1,12 @@
+console.log("Mensaje oculto: Una taza alivia una mañana");
+/* Mensaje consola sacado de <a href="https://www.w3schools.com/jsref/prop_win_console.asp"> Window Console Object </a> */
 
-const button = document.createElement('button'); 
-button.type = 'button'; 
-button.innerText = 'Pinchame' ; 
-document.body.appendChild(button); 
+let x = 1
+const mostrar = (i) => {console.log(x+i)}
+const tacita = (a) => {console.log (x-a)}
+/* Intento de aprender Javascript mediante la programación de ejercicios matemáticos con mi pololo, que es programador */
+
+document.getElementById("btn").addEventListener ("click", e=>{
+window.location.href = "https://ko-fi.com/"
+/* Botón sacado de <a href="https://www.youtube.com/watch?v=8frJ_4n3s-A&ab_channel=CodeaApp"> Cómo crear un botón con Html, CSS y Javascript </a> */
+})
