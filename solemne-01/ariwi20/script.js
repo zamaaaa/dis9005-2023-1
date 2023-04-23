64f02e8ea78e3d684efc6858bd1f2332a24f2618
Aranza Frias <128400193+ariwi20@users.noreply.github.com>
2023-04-23 16:34:33 -0400

    Update script.js

--- solemne-01/ariwi20/script.js
+++ solemne-01/ariwi20/script.js
@@ -1,10 +1,3 @@
-console.log("Atrapalos a todos!");
-console.log("ancho:" + window.innerWidth);
-let par;
-par = document.getElementById("atrapalos");
-if (window.innerWidth < 800)  {
-atrapalos.innerHTML =  "atrapalos en celular";
-}
-else {
-atrapalos.innerHTML =  "atrapalos en desktop";
-}
+console.log("¡Atrapalos a todos!");
+//referencia de https://www.w3schools.com/jsref/met_console_log.asp
+
