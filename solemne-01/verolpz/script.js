0f1c4009fe2b27674c73582d45a87ccbb217bdae
verolpz <128399630+verolpz@users.noreply.github.com>
2023-04-23 16:35:23 -0400

    Update script.js

--- solemne-01/verolpz/script.js
+++ solemne-01/verolpz/script.js
@@ -1,1 +1,11 @@
 console.log("Hola! bienvenido");
+
+//Códigos extraídos del trabajo de una compañera, se añadió fecha y ancho de ventana//
+console.log("ancho: " + window.innerWidth);
+const d = new Date("2022-03-25");
+document.getElementById("fecha").innerHTML=d;
+function myFunction() {
+    document.getElementById("fecha").style.fontSize = "25px"; 
+    document.getElementById("fecha").style.color = "red";
+    document.getElementById("fecha").style.backgroundColor = "yellow";        
+  }
