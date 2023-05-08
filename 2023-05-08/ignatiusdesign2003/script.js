cbdb29df29e0a69728c67f2b4703730c3462ef29
ignatiusdesign2003 <128399461+ignatiusdesign2003@users.noreply.github.com>
2023-05-08 12:49:59 -0400

    Add files via upload

---
+++ 2023-05-08/ignatiusdesign2003/script.js
@@ -0,0 +1,14 @@
+console.log("probando");
+let martha = document.getElementById("inicio");
+console.log (martha)
+
+martha.addEventListener ("click" , cambiarcolor);
+martha.addEventListener ("mouseup" , cambiarTamano);
+
+function cambiarcolor (){
+    console.log("probando");
+    martha.style.backgroundColor = "red" ;
+}
+function cambiarTamano(){
+    console.log("otro");
+}
