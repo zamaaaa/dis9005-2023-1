0a26370555e86a7b601fd6ae1e7cefe12cf41be7
JoacoAhumada <128399586+JoacoAhumada@users.noreply.github.com>
2023-05-08 12:50:51 -0400

    Add files via upload

---
+++ 2023-05-08/JoacoAhumada/script.js
@@ -0,0 +1,15 @@
+console.log("probando")
+let martha = document.getElementById("inicio");
+console.log(martha);
+
+martha.addEventListener("click", cambiaColor);
+martha.addEventListener("mouseup", cambiarTamano);
+
+function cambiaColor(){
+    console.log("probando");
+    martha.style.backgroundColor = "pink"
+}
+
+function cambiarTamano(){
+    console.log("otro");
+}
