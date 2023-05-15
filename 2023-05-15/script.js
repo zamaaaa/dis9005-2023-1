9f3d0fc2578b1f6f132465dfb6e61e9699b0e1e9
Yeyeezz <128399785+Yeyeezz@users.noreply.github.com>
2023-05-15 12:48:23 -0400

    Add files via upload

---
+++ 2023-05-15/script.js
@@ -0,0 +1,23 @@
+console.log ("probando");
+let botonInicio = document.getElementById("inicio");
+console.log(botonInicio);
+
+botonInicio.addEventListener("click", cambiarColor);
+botonInicio.addEventListener("mousedown", cambiarTamano);
+
+function cambiarColor(){
+    console.log("probando");
+    botonInicio.style.backgroundColor = "red";
+}
+function cambiarTamano(){
+    console.log("otro");
+}
+
+let formulario = document.getElementById("formulario");
+console.log(formulario);
+formulario.addEventListener("submit", grabarInfo);
+
+function grabarInfo(){
+    let apellido = document.getElementById("lname");
+    alert(apellido.value);
+}
