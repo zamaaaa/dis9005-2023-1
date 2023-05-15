ee1eff0ad5b61416c9333a1b8bc08e96d7229af5
menteneon <128399807+menteneon@users.noreply.github.com>
2023-05-15 12:39:26 -0400

    Add files via upload

---
+++ 2023-05-15/script.js
@@ -0,0 +1,25 @@
+console.log ("proban 1, 2, 3");
+let juanin= document.getElementById("inicio");
+console.log(juanin);
+
+
+juanin.addEventListener("click", cambiarColor);
+juanin.addEventListener("mousedown", cambiarTamano);
+function cambiarColor(){
+    console.log("probando");
+    juanin.style.backgroundColor="red";
+}
+
+function cambiarTamano(){
+    console.log("otro");
+}
+
+let formulario = document.getElementById("formulario");
+console.log(formulario);
+formulario.addEventListener("submit", grabarInfo);
+
+function grabarInfo(){
+    let apellido = document.getElementById("lname");
+    alert(apellido.value);
+}
+//cambiar tamaño y cambiar color solo son nombres ficticios, no affectan en el codigo//
