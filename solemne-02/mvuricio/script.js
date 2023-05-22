04df55f0a1aa277a98d1a597a2d1f7614d8d0a72
mvuricio <128399514+mvuricio@users.noreply.github.com>
2023-05-22 12:52:43 -0400

    Add files via upload

---
+++ solemne-02/mvuricio/script.js
@@ -0,0 +1,18 @@
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate(event) {
+    event.preventDefault();
+  document.getElementById("demo").innerHTML = Date();
+}
+
+
+function go(){
+        
+    if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+    } 
+   
