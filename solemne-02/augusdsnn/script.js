b10ae70bf89cbc98aa5470f4587ee3cb2170ecf4
augusdsnn <128400710+augusdsnn@users.noreply.github.com>
2023-05-22 12:57:09 -0400

    Add files via upload

---
+++ solemne-02/augusdsnn/script.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+// controles Avanzar/retroceder
+function plisSlides (n) {
+    showSlides(slideIndex += n);
+}
+// Control de previsualización de fotos
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += "active"
+}
