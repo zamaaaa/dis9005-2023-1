4f9f4e59537d14a1076474ffa2496ff519a96d98
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-05-23 09:28:45 -0400

    agregar comentarios en JS

--- solemne-02/anaisoa/script.js
+++ solemne-02/anaisoa/script.js
@@ -1,5 +1,12 @@
 console.log("Conoce sobre tu destino");
 // Me sale un error en la consola que no pude resolver
+
+// lo resolvio profesore, habia un tag de picture que no habias cerrado
+// y tambien un warning de que te faltaba definir el idioma, por eso agregue
+// el tag meta con charset
+// por favor incluir comentarios sobre lo que estas haciendo aqui
+// y tambien las fuentes, porque esto lo vimos en clases
+
 let formulario = document.getElementById("formulario");
 console.log(formulario);
 
@@ -8,7 +15,8 @@ formulario.addEventListener("submit", ingresar);
 function ingresar(event) {
   event.preventDefault();
   console.log("has ingresado");
-  document.getElementById("bienvenida").innerHTML = "hola tu " + nombre.value;
+  document.getElementById("bienvenida").innerHTML =
+    "hola tu " + nombre.value;
 }
 
 console.log("probando");
