cad1dbc46ebd3df4b99decec5897be3362f9dd59
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-05-23 09:43:57 -0400

    agregar comentarios con correcciones

--- solemne-02/ariwi20/script.js
+++ solemne-02/ariwi20/script.js
@@ -1,16 +1,24 @@
-
-document.getElementById("myBtn").addEventListener("click", function() {
-alert("Elige el que mejor se adapte a tu personalidad");
-})
+document
+  .getElementById("myBtn")
+  .addEventListener("click", function () {
+    alert("Elige el que mejor se adapte a tu personalidad");
+  });
 console.log("probando");
 let martha = document.getElementById("myBtn");
-console.log(martha)
+console.log(martha);
 
-martha.addEventListener('myBtn', cambiarColor);
-martha.addEventListener('mousedown', cambiarTamano);
+martha.addEventListener("myBtn", cambiarColor);
 
 function cambiarColor() {
-  console.log('probando');
-  martha.style.backgroundColor = 'pink';
+  console.log("probando");
+  martha.style.backgroundColor = "pink";
 }
 
+// esto estaba en tu index.html y lo movi aca
+// el formateo que hice es que cada punto . empieza en una nueva linea
+// es solamente visual para entender los pasos, no afecta funcionamiento
+document
+  .getElementById("myBtn")
+  .addEventListener("click", function () {
+    alert("Elige el que mejor se adapte a tu personalidad");
+  });
