d94ff76861735d1ad56d7ae944dc2c29d29d57a3
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-05-24 08:30:39 -0400

    agregar comentarios y formateo

--- solemne-02/CristobalBustamante/script.js
+++ solemne-02/CristobalBustamante/script.js
@@ -1,8 +1,14 @@
-console.log('Welcome to Mercy Howlett Portfolio');
-//Comando para ocultar puntero//
-document.body.style.cursor = 'none';
-//Comando de nuevo puntero//
-const cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', e => {
-cursor.setAttribute("style",`top: ${e.pageY - 0}px; left: ${e.pageX - 0}px;`);
-})
+console.log("Welcome to Mercy Howlett Portfolio");
+
+// Comando para ocultar puntero
+document.body.style.cursor = "none";
+
+// Comando de nuevo puntero
+// fuente?
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    `top: ${e.pageY - 0}px; left: ${e.pageX - 0}px;`,
+  );
+});
