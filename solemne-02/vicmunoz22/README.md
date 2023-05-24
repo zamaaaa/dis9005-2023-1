d2e0d80518e6eed527bdc198120001bb6cfad4f5
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-05-24 10:39:04 -0400

    agregar formateo

--- solemne-02/vicmunoz22/README.md
+++ solemne-02/vicmunoz22/README.md
@@ -1,29 +1,17 @@
 # solemne-02
 
-ir a mi fork del repositorio, y hacer click en "sync fork"
-
-![pantallazo de sync fork](./github-sync-fork.jpg)
-
-luego hacer click en el botón verde "update branch"
-
-![pantallazo de update branch](./github-update-branch.jpg)
-
-con esto lograremos que nuestro fork esté al día con el original ("up to date")
-
-![pantallazo de up to date](./github-up-to-date.jpg)
-
-![pantallazo](./captura1.png)
-![pantallazo](./captura2.png)
-![pantallazo](./captura3.png)
-![pantallazo](./captura4.png)
-![pantallazo](./captura5.png)
-![pantallazo](./captura6.png)
-![pantallazo](./captura7.png)
-![pantallazo](./captura8.png)
-![pantallazo](./captura9.png)
-![pantallazo](./captura10.png)
-![pantallazo](./captura11.png)
-![pantallazo](./captura12.png)
-![pantallazo](./captura13.png)
-![pantallazo](./captura14.png)
-![pantallazo](./captura15.png)
+![pantallazo 1](./captura1.png)
+![pantallazo 2](./captura2.png)
+![pantallazo 3](./captura3.png)
+![pantallazo 4](./captura4.png)
+![pantallazo 5](./captura5.png)
+![pantallazo] 6(./captura6.png)
+![pantallazo 7](./captura7.png)
+![pantallazo 8](./captura8.png)
+![pantallazo 9](./captura9.png)
+![pantallazo 10](./captura10.png)
+![pantallazo 11](./captura11.png)
+![pantallazo 12](./captura12.png)
+![pantallazo 13](./captura13.png)
+![pantallazo 14](./captura14.png)
+![pantallazo 15](./captura15.png)
--- solemne-02/vicmunoz22/script.js
+++ solemne-02/vicmunoz22/script.js
@@ -3,32 +3,31 @@ console.log("ancho: " + window.innerWidth);
 const d = new Date("2022-03-25");
 document.getElementById("fecha").innerHTML = d;
 function myFunction() {
-    document.getElementById("fecha").style.fontSize = "25px"; 
-    document.getElementById("fecha").style.color = "red";
-    document.getElementById("fecha").style.backgroundColor = "yellow";        
-  }
-  console.log("probando");
+  document.getElementById("fecha").style.fontSize = "25px";
+  document.getElementById("fecha").style.color = "red";
+  document.getElementById("fecha").style.backgroundColor = "yellow";
+}
+console.log("probando");
 let martha = document.getElementById("inicio");
 console.log(martha);
 
 martha.addEventListener("click", cambiarColor);
-martha.addEventListener("mousedown", cambiarTamano);
 
 function cambiarColor() {
   console.log("probando");
   martha.style.backgroundColor = "pink";
 }
-function cambiarTamano() {
-  console.log("otro");
+
+function myFunction() {
+  var x = document.getElementById("myInput").value;
+  document.getElementById("demo").innerHTML = "You wrote: " + x;
 }
 function myFunction() {
-    var x = document.getElementById("myInput").value;
-    document.getElementById("demo").innerHTML = "You wrote: " + x;
-  }
-  function myFunction() {
-    alert("The form was submitted");
-  }
-  document.getElementById("myForm").onsubmit = function() {myFunction()};
+  alert("The form was submitted");
+}
+document.getElementById("myForm").onsubmit = function () {
+  myFunction();
+};
 
 function myFunction() {
   alert("The form was submitted");
@@ -43,5 +42,6 @@ formulario.addEventListener("submit", Ingresar);
 function Ingresar(event) {
   event.preventDefault();
   console.log("has ingresado");
-  document.getElementById("bienvenida").innerHTML = "hola tu " + Nombre.value;
+  document.getElementById("bienvenida").innerHTML =
+    "hola tu " + Nombre.value;
 }
