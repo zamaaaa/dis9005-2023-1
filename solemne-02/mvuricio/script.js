7376702ff70322e497bf07dda1a88da2c54017c0
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-05-24 10:06:57 -0400

    agregar comentarios y formato

--- solemne-02/mvuricio/script.js
+++ solemne-02/mvuricio/script.js
@@ -1,18 +1,27 @@
-document.getElementById("myBtn").addEventListener("click", displayDate);
+document
+  .getElementById("myBtn")
+  .addEventListener("click", displayDate);
 
 function displayDate(event) {
-    event.preventDefault();
+  // esto solamente lo recomiendo usar para el event listener del
+  // evento submit hecho en un formulario
+  event.preventDefault();
+  // por favor procesa la info de Date de alguna manera
   document.getElementById("demo").innerHTML = Date();
 }
 
-
-function go(){
-        
-    if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
-            document.form.submit(); 
-        } 
-        else{ 
-             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
-        } 
-    } 
-   
+// darle un nombre mas explicativo a go
+// y explicar como se esta usando
+function go() {
+  if (
+    document.form.password.value == "CONTRASEÑA" &&
+    document.form.login.value == "USUARIO"
+  ) {
+    document.form.submit();
+  } else {
+    alert(
+      // corregi este por favor, decia "porfavor"
+      "Por favor ingrese, nombre de usuario y contraseña correctos.",
+    );
+  }
+}
