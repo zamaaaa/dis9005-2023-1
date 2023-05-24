7261524d847edb6f982bebd2cf1b1abbf8a3886d
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-05-24 10:28:17 -0400

    agregar comentarios y formateo

--- solemne-02/Tessbb/README.md
+++ solemne-02/Tessbb/README.md
@@ -1,20 +1,8 @@
 # solemne-02
 
-ir a mi fork del repositorio, y hacer click en "sync fork"
-
-![pantallazo de sync fork](./github-sync-fork.jpg)
-
-luego hacer click en el botón verde "update branch"
-
-![pantallazo de update branch](./github-update-branch.jpg)
-
-con esto lograremos que nuestro fork esté al día con el original ("up to date")
-
-![pantallazo de up to date](./github-up-to-date.jpg)
-
 ![pantallazo 1](./cap-1.png)
-![pantallazo 1](./cap-2.png)
-![pantallazo 1](./cap-3.png)
-![pantallazo 1](./cap-4.png)
-![pantallazo 1](./cap-5.png)
-![pantallazo 1](./cap-6.png)
+![pantallazo 2](./cap-2.png)
+![pantallazo 3](./cap-3.png)
+![pantallazo 4](./cap-4.png)
+![pantallazo 5](./cap-5.png)
+![pantallazo 6](./cap-6.png)
--- solemne-02/Tessbb/script.js
+++ solemne-02/Tessbb/script.js
@@ -1,5 +1,3 @@
-console.log("123");
-
 let formulario = document.getElementById("formulario");
 console.log(formulario);
 
@@ -9,11 +7,13 @@ formulario.addEventListener("submit", ingresar);
 function ingresar(event) {
   event.preventDefault();
   console.log("has ingresado");
-  alert("Ya ingresaste\nAhora debes esperar un poco\nApenas podamos te responderemos")
+  alert(
+    "Ya ingresaste\nAhora debes esperar un poco\nApenas podamos te responderemos",
+  );
 }
 // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_alert2 esto es par auqe la alerta se divida en dos lineas
 
 // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_head_script esto es para ponerle texto antes de un boton y que cuando se haga onclick cambie el parrafo
 function myFunction() {
-    document.getElementById("demo").innerHTML = "adoptar";
-  }
+  document.getElementById("demo").innerHTML = "adoptar";
+}
