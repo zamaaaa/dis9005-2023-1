2ef72e38a882c3f5a3286687b1423e2cf9fef98e
magdalena valdivia <128399618+mgdalol@users.noreply.github.com>
2023-05-29 12:28:41 -0400

    Update script.js

--- solemne-02/mgdalol/script.js
+++ solemne-02/mgdalol/script.js
@@ -1,7 +1,7 @@
 console.log("Hola solemne 2 tratame bien");
 console.log("ancho: " + window.innerWidth);
-
+// como cambiar el tamaño y color de los textos sin cambiar el css a continuacion
 document.getElementById("personaje").style.color = "white";
 document.getElementById("personaje").style.fontSize = "20px";
-document.getElementById("personaje").style.fontFamily =
-  "Georgia, 'Times New Roman', Times, serif";
+document.getElementById("personaje").style.fontFamily = "Georgia, 'Times New Roman', Times, serif";
+// importante agregar id dentro del texto que quiera editar para que sea encontrado dentro del js
