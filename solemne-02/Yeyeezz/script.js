e7411c31f9e4f574cc6a7a4a790483e031b89374
Yeyeezz <128399785+Yeyeezz@users.noreply.github.com>
2023-05-29 11:30:39 -0400

    Update script.js

--- solemne-02/Yeyeezz/script.js
+++ solemne-02/Yeyeezz/script.js
@@ -2,24 +2,25 @@ let formulario = document.getElementById("completar");
 console.log(formulario);
 formulario.addEventListener("submit", grabarInfo);
 
+// formulario para completar la letra de la canci&oacute;n 
+
 function grabarInfo() {
-  let confirmar = document.getElementById("letra");
-  alert("Muy bien?");
+    let confirmar = document.getElementById("letra");
+    alert("Muy bien?");
 }
 function respuesta() {
-  document.getElementById("respuesta").innerHTML =
-    "It's always pulling us apart";
+    document.getElementById("respuesta").innerHTML = "It's always pulling us apart";
 }
 function cambiarColor() {
-  document.getElementById("cambiarColor").style.color = "green";
+    document.getElementById("cambiarColor").style.color = "green";
 }
-function cambiarColor1() {
-  document.getElementById("cambiarColor1").style.color = "green";
+function verde() {
+    document.getElementById("verde").style.color = "green";
 }
-// Cambiar color texto https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_onclick2
+// Cambiar color de texto al hacer click en la frase https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_onclick2
 
 function sorpresa() {
-  document.body.style.backgroundColor = "salmon";
-  document.body.style.color = "purple";
+    document.body.style.backgroundColor = "salmon";
+    document.body.style.color = "purple"
 }
-// cambiar color letra y pantalla https://bobbyhadz.com/blog/javascript-change-background-color-on-click
+// cambiar color, letra y pantalla al hacer click en bot&oacute;n https://bobbyhadz.com/blog/javascript-change-background-color-on-click
