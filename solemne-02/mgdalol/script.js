d7ab010120ecc9fc138e80ff3e16b0d557309e85
magdalena valdivia <128399618+mgdalol@users.noreply.github.com>
2023-05-29 12:47:33 -0400

    Update script.js

--- solemne-02/mgdalol/script.js
+++ solemne-02/mgdalol/script.js
@@ -5,3 +5,16 @@ document.getElementById("personaje").style.color = "white";
 document.getElementById("personaje").style.fontSize = "20px";
 document.getElementById("personaje").style.fontFamily = "Georgia, 'Times New Roman', Times, serif";
 // importante agregar id dentro del texto que quiera editar para que sea encontrado dentro del js
+
+let formulario = document.getElementById("formulario");
+console.log(formulario);
+
+formulario.addEventListener("submit", registrarme);
+function registrarme(event) {
+    event.preventDefault();
+    console.log("haz añadido tu personaje");
+    document.getElementById("guardado").innerHTML = 
+    "hola" + escribeloaqui.value;
+    // intente hacer el form pero no funciona la respuesta despues de enviarlo
+    // el formulario lo obtuve por mi compañera sofia graciasuwu
+ }
