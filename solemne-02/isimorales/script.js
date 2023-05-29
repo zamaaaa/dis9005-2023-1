3a0913c0fa6a8c897e4be5179dc50a3a62c320b3
isimorales <128399508+isimorales@users.noreply.github.com>
2023-05-29 12:46:59 -0400

    Update script.js

--- solemne-02/isimorales/script.js
+++ solemne-02/isimorales/script.js
@@ -1,4 +1,3 @@
-
 console.log("123");
 
 let formulario = document.getElementById("formulario");
@@ -7,56 +6,40 @@ console.log(formulario);
 formulario.addEventListener("submit", ingresar);
 
 function ingresar(event) {
-  event.preventDefault();
-  console.log("has ingresado");
-  document.getElementById("bienvenida").innerHTML = "hola belleza " + nombre.value;
+    event.preventDefault();
+    console.log("has ingresado");
+    document.getElementById("bienvenida").innerHTML = "hola belleza " + nombre.value;
 }
 
+// PARA VER LOS MENSAJES EN CONSOLA SE HACE.... option comando i
 
 // para un mensaje cuantico
-console.log ( "mensaje" ); //Mostrará el mensaje en forma de error, resaltado en rojo.
+console.log("mensaje"); //Mostrará el mensaje en forma de error, resaltado en rojo.
 
 
 
-let day; 
+let day;
 // codigo extraido de  https://www.w3schools.com/js/tryit.asp?filename=tryjs_switch
 switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-    day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case  6:
-    day = "Saturday";
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
 }
 document.getElementById("demo").innerHTML = "Today is " + day;
-
-
-// mouseX and mouseY
-
-strokeWeight(3);
-stroke(57, 0, 214);
-fill(0, 210, 247);
-
-
-draw = function() {
-    
-    
-    background(255, 255, 255);
-    
-    ellipse(mouseY, mouseX, 30, 30);
-    
-};
