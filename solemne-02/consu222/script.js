ace3ada997577e7c23b1bd2824593e97fddc20e7
consu222 <128386532+consu222@users.noreply.github.com>
2023-05-29 11:23:09 -0400

    Add files via upload

--- solemne-02/consu222/script.js
+++ solemne-02/consu222/script.js
@@ -2,7 +2,7 @@ console.log("probando");
 let botonInicio = document.getElementById("inicio");
 console.log(botonInicio);
 
-botonInicio.addEventListener("click", cambiarColor);
+// botonInicio.addEventListener("click", cambiarColor);
 
 function cambiarColor() {
   console.log("probando");
@@ -11,12 +11,22 @@ function cambiarColor() {
 
 let formulario = document.getElementById("formulario");
 console.log(formulario);
-formulario.addEventListener("submit", grabarInfo);
+// formulario.addEventListener("submit", grabarInfo);
 
 function grabarInfo() {
   let apellido = document.getElementById("lname");
   alert(apellido.value);
 }
+
+formulario.addEventListener("submit", ingresar);
+
+function ingresar(event) {
+  event.preventDefault();
+  console.log("has ingresado");
+  document.getElementById("bienvenida").innerHTML = "hola tu " + nombre.value;
+}
+// Ocupe el formulario basico, para probar si funcionaba :)
+
 
 // <><h1>My First JavaScript</h1><button type="button"
 //  onclick="document.getElementById('demo').innerHTML = Date()">
