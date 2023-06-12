b718d851a657d59594855fe5945ae8c5532f0a51
consu222 <128386532+consu222@users.noreply.github.com>
2023-06-12 12:31:27 -0400

    Add files via upload

---
+++ examen/consu222/script 2.js
@@ -0,0 +1,34 @@
+console.log("probando");
+let botonInicio = document.getElementById("inicio");
+console.log(botonInicio);
+
+botonInicio.addEventListener("click", cambiarColor);
+botonInicio.addEventListener("mousedown", cambiarTamano);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
